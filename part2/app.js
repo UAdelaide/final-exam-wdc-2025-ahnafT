eca9e6e01a74fd7a5206d24acd1c78e0a2c8b507
ahnafT <ahnaf.talukder@student.adelaide.edu.au>
2025-06-20 14:32:46 +0000

    [automatic commit] 2025-06-20 14:32:45 UTC 14.203.76.178

--- part2/app.js
+++ part2/app.js
@@ -27,7 +27,7 @@ let db;
 
 (async () => {
   try {
-    // ✅ Connect to DB
+    //Connect to DB
     db = await mysql.createConnection({
       host: 'localhost',
       user: 'root',
@@ -35,7 +35,7 @@ let db;
       database: 'DogWalkService'
     });
 
-    // ✅ Now that db is defined, define your custom /api/dogs route here
+    //dog details
     app.get('/api/dogs', async (req, res) => {
       try {
         const [dogs] = await db.query(`
